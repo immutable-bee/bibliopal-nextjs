f022e5227e0ea152c5806deef7c9543d44f39e5f
immutable-bee <immutablebee@gmail.com>
2023-06-14 15:35:57 -0600

    add logo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../styles/index.css";
+import "../styles/App.css";
 import { MantineProvider } from "@mantine/core";
 import TableDataProvider from "../context/TableDataContext";
 import { AppProps } from "next/app";
