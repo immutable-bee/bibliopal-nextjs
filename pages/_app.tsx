2a08d45827cc4ac5aa2a17a738357d484e5b21ff
SoftronetIbrahim <108011994+SoftronetIbrahim@users.noreply.github.com>
2023-06-21 17:01:17 +0500

    profile page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,82 +3,123 @@ import "../styles/App.css";
 import { MantineProvider } from "@mantine/core";
 import TableDataProvider from "../context/TableDataContext";
 import { AppProps } from "next/app";
+import Link from "next/link";
+import Logo from "../public/logo.png";
+import Image from "next/image";
+import { useRouter } from "next/router";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  const linkColor = (path: string) => {
+    return router.pathname === path ? "#2EAAED" : "#828282";
+  };
   return (
     <React.StrictMode>
-      <MantineProvider
-        theme={{
-          primaryColor: "yellow",
-          defaultRadius: "md",
-          colors: {
-            yellow: [
-              "#ffffcc",
-              "#ffff99",
-              "#ffff66",
-              "#ffff33",
-              "#ffff00",
-              "#ffcc00",
-              "#ffc71f",
-              "#ffbf00",
-              "#ffb300",
-              "#ffa500",
-            ],
-            blue: [
-              "#E4F3FF",
-              "#C4E1FF",
-              "#A3CEFF",
-              "#81BAFF",
-              "#60A7FF",
-              "#3E93FF",
-              "#2EAAED",
-              "#1D91DA",
-              "#0D78C8",
-              "#006FB5",
-            ],
-          },
-          components: {
-            TextInput: {
-              classNames: {
-                input: "input",
-                root: "input-root",
-                wrapper: "input-wrapper",
-              },
+      <div className="bg-[#FEFBE8] min-h-screen">
+        <header className="w-full flex justify-between items-center px-8 py-4">
+          <Image src={Logo} alt="Logo" className="w-20 sm:w-36" />
+
+          <nav className="text-center">
+            <Link href="/">
+              <span
+                style={{ color: linkColor("/") }}
+                className="mx-2 font-medium sm:mx-4 text-base sm:text-xl"
+              >
+                Home
+              </span>
+            </Link>
+            <Link href="/listing">
+              <span
+                style={{ color: linkColor("/listing") }}
+                className="mx-2 font-medium sm:mx-4 text-base sm:text-xl"
+              >
+                Listing
+              </span>
+            </Link>
+            <Link href="/profile">
+              <span
+                style={{ color: linkColor("/profile") }}
+                className="mx-2 font-medium sm:mx-4 text-base sm:text-xl"
+              >
+                Profile
+              </span>
+            </Link>
+          </nav>
+        </header>
+        <MantineProvider
+          theme={{
+            primaryColor: "yellow",
+            defaultRadius: "md",
+            colors: {
+              yellow: [
+                "#ffffcc",
+                "#ffff99",
+                "#ffff66",
+                "#ffff33",
+                "#ffff00",
+                "#ffcc00",
+                "#ffc71f",
+                "#ffbf00",
+                "#ffb300",
+                "#ffa500",
+              ],
+              blue: [
+                "#E4F3FF",
+                "#C4E1FF",
+                "#A3CEFF",
+                "#81BAFF",
+                "#60A7FF",
+                "#3E93FF",
+                "#2EAAED",
+                "#1D91DA",
+                "#0D78C8",
+                "#006FB5",
+              ],
             },
-            NumberInput: {
-              classNames: {
-                input: "input",
-                root: "input-root",
-                wrapper: "input-wrapper",
-              },
-            },
-            Textarea: {
-              classNames: {
-                input: "input",
-                root: "input-root",
-                wrapper: "input-wrapper",
-              },
-            },
-            Button: {
-              defaultProps: { radius: "xl", c: "gray.9", fz: "md" },
-              styles: (theme) => ({
-                root: {
-                  border: "2px solid",
-                  borderColor: theme.black,
+            components: {
+              TextInput: {
+                classNames: {
+                  input: "input",
+                  root: "input-root",
+                  wrapper: "input-wrapper",
                 },
-              }),
+              },
+              NumberInput: {
+                classNames: {
+                  input: "input",
+                  root: "input-root",
+                  wrapper: "input-wrapper",
+                },
+              },
+              Textarea: {
+                classNames: {
+                  input: "input",
+                  root: "input-root",
+                  wrapper: "input-wrapper",
+                },
+              },
+              Button: {
+                defaultProps: { radius: "xl", c: "gray.9", fz: "md" },
+                styles: (theme) => ({
+                  root: {
+                    border: "2px solid",
+                    borderColor: theme.black,
+                  },
+                }),
+              },
             },
-          },
-        }}
-        withCSSVariables
-        withGlobalStyles
-        withNormalizeCSS
-      >
-        <TableDataProvider>
-          <Component {...pageProps} />
-        </TableDataProvider>
-      </MantineProvider>
+          }}
+          withCSSVariables
+          withGlobalStyles
+          withNormalizeCSS
+        >
+          <TableDataProvider>
+            <Component {...pageProps} />
+          </TableDataProvider>
+        </MantineProvider>
+      </div>
     </React.StrictMode>
   );
 }
---
+++ pages/profile.tsx
@@ -0,0 +1,165 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+import Link from "next/link";
+import Logo from "../public/logo.png";
+// import '../styles/tailwind.css'
+
+export default function Home() {
+  const [storeName, setStoreName] = useState("");
+  const [storeType, setStoreType] = useState("");
+  const [email, setEmail] = useState("");
+  const [address, setAddress] = useState("");
+  const [url, setUrl] = useState("");
+  const [autoUpload, setAutoUpload] = useState(false);
+  const [displayContactInfo, setDisplayContactInfo] = useState(false);
+  const [visibility, setVisibility] = useState(false);
+  const [planOptions, setPlanOptions] = useState("Free");
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    console.log({
+      storeName,
+      storeType,
+      email,
+      address,
+      url,
+      autoUpload,
+      displayContactInfo,
+      visibility,
+      planOptions,
+    });
+  };
+
+  return (
+    <div className="h-full">
+      <div className="h-full flex flex-col items-center justify-center">
+        <div className="max-w-xl w-full bg-whit px-4 sm:px-8 py-3 sm:py-6 rounded">
+          <h1 className="text-lg sm:text-2xl font-medium text-center ">
+            Profile Page
+          </h1>
+          <form onSubmit={handleSubmit} className="mt-2 sm:mt-6">
+            <div className="py-2">
+              <label className="text-sm text-gray-700">Store name</label>
+              <input
+                type="text"
+                className="w-full rounded-lg bg-gray-100 px-4 my-1 py-2"
+                value={storeName}
+                onChange={(e) => setStoreName(e.target.value)}
+              />
+            </div>
+            <div className="py-2">
+              <label className="text-sm text-gray-700">Store Type</label>
+              <select
+                className="form-select w-full rounded-lg bg-gray-100 px-4 my-1 py-2"
+                value={storeType}
+                onChange={(e) => setStoreType(e.target.value)}
+              >
+                <option value="1">1</option>
+              </select>
+            </div>
+            <div className="py-2">
+              <label className="text-sm text-gray-700">Email</label>
+              <input
+                type="email"
+                className="w-full rounded-lg bg-gray-100 px-4 my-1 py-2"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="py-2">
+              <label className="text-sm text-gray-700">Address</label>
+              <input
+                type="text"
+                className="w-full rounded-lg bg-gray-100 px-4 my-1 py-2"
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+              />
+            </div>
+            <div className="py-2">
+              <label className="text-sm text-gray-700">URL</label>
+              <input
+                type="url"
+                className="w-full rounded-lg bg-gray-100 px-4 my-1 py-2"
+                value={url}
+                onChange={(e) => setUrl(e.target.value)}
+              />
+            </div>
+
+            <label className="relative mt-6 flex items-center cursor-pointer">
+              <input
+                type="checkbox"
+                value=""
+                className="sr-only peer"
+                checked={autoUpload}
+                onChange={() => setAutoUpload(!autoUpload)}
+              />
+              <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-[#2EAAED]"></div>
+              <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+                Auto upload
+              </span>
+            </label>
+            <label className="relative mt-6 flex items-center cursor-pointer">
+              <input
+                type="checkbox"
+                value=""
+                className="sr-only peer"
+                checked={displayContactInfo}
+                onChange={() => setDisplayContactInfo(!displayContactInfo)}
+              />
+              <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-[#2EAAED]"></div>
+              <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+                Display contact info
+              </span>
+            </label>
+
+            <div className="mt-4">
+              <label className="flex items-center">
+                <input
+                  type="checkbox"
+                  className="form-checkbox ml-1 rounded-md h-6 w-6 text-[#2EAAED]"
+                  checked={visibility}
+                  onChange={() => setVisibility(!visibility)}
+                />
+                <span className="ml-6 text-gray-700">30-day visibility</span>
+              </label>
+            </div>
+
+            {/* <div className='mt-4'>
+    <span className="text-lg text-gray-900 font-medium">Plan options:</span>
+    <div className="mt-2">
+        <label className="flex items-center py-1.5">
+            <input type="radio" className="form-radio h-6 w-6 text-[#2EAAED]" value="Free" checked={planOptions === 'Free'} onChange={() => setPlanOptions('Free')} />
+            <span className="ml-2 text-gray-700">Free</span>
+        </label>
+        <label className="flex items-center py-1.5">
+            <input type="radio" className="form-radio h-6 w-6 text-[#2EAAED]" value="Tier 1" checked={planOptions === 'Tier 1'} onChange={() => setPlanOptions('Tier 1')} />
+            <span className="ml-2 text-gray-700">Tier 1</span>
+        </label>
+        <label className="flex items-center py-1.5">
+            <input type="radio" className="form-radio h-6 w-6 text-[#2EAAED]" value="Tier 2" checked={planOptions === 'Tier 2'} onChange={() => setPlanOptions('Tier 2')} />
+            <span className="ml-2 text-gray-700">Tier 2</span>
+        </label>
+    </div>
+</div> */}
+            <div className="mt-4">
+              <button
+                onClick={() => alert("Manage Subscription")}
+                className="w-full rounded-lg bg-[#2EAAED] py-2 text-white text-center font-bold"
+              >
+                Manage Subscription
+              </button>
+            </div>
+            <button
+              type="submit"
+              className="w-full mt-6 rounded-lg bg-[#2EAAED] py-2 text-white text-center font-bold"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
