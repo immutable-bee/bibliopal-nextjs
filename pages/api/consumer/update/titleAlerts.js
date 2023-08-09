8f18ecac03a87d5410c83f763683f741cef4ad5c
immutable-bee <immutablebee@gmail.com>
2023-08-09 15:53:51 -0600

    add route

---
+++ pages/api/consumer/update/titleAlerts.js
@@ -0,0 +1,51 @@
+import { prisma } from "../../../../db/prismaDB";
+
+const handler = async (req, res) => {
+  const { email, title, type } = req.body;
+
+  const titleToRemove = title;
+
+  switch (type) {
+    case "add":
+      try {
+        await prisma.consumer.update({
+          where: { email: email },
+          data: {
+            tracked_titles: {
+              push: title,
+            },
+          },
+        });
+        res.status(200).json;
+      } catch (error) {
+        res.status(500).json({ message: error.message });
+      }
+    case "delete":
+      try {
+        const consumer = await prisma.consumer.findUnique({
+          where: { email: email },
+          select: {
+            tracked_titles: true,
+          },
+        });
+
+        const updatedTitles = consumer.tracked_titles.filter(
+          (title) => title !== titleToRemove
+        );
+
+        const updateConsumer = await prisma.consumer.update({
+          where: { email: email },
+          data: {
+            tracked_titles: updatedTitles,
+          },
+        });
+        res.status(200).json;
+      } catch (error) {
+        res.status(500).json({ message: error.message });
+      }
+    default:
+      res.status(400).json({ message: "Invalid type or no type provided" });
+  }
+};
+
+export default handler;
