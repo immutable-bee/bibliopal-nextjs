dd0d8ca14fb8eee35dc0937e889c00abad581a1d
immutable-bee <immutablebee@gmail.com>
2023-09-04 15:17:29 -0600

    add tooltip

--- components/customer/profile/Alerts.js
+++ components/customer/profile/Alerts.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import Image from "next/image";
+import TooltipComponent from "../../utility/Tooltip";
 
 const Alerts = ({ props, fetchUserData }) => {
   const [newTitle, setNewTitle] = useState("");
@@ -224,10 +225,36 @@ const Alerts = ({ props, fetchUserData }) => {
       </div>
 
       <div>
-        <div>
+        <div className="flex items-center">
           <h3 className="text-xl mt-5 sm:mt-16 font-medium">
             Book alert zip codes
           </h3>
+          <div className="mt-16">
+            <TooltipComponent
+              rounded
+              width="!w-64"
+              id="shipping-status-tooltip"
+              css={{ zIndex: 10000 }}
+              content={
+                "Adding zip code(s) will limit matches to stores in the provided zip code(s). This is strongly recommended."
+              }
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-8 h-8 ml-3 cursor-pointer"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z"
+                />
+              </svg>
+            </TooltipComponent>
+          </div>
         </div>
         <div className="mt-4">
           <label className="text-sm text-black font-medium">Zip Code</label>
