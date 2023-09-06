7dd3ce00f9edb09204c3fc0065562239fe5e7262
immutable-bee <immutablebee@gmail.com>
2023-09-06 16:40:19 -0600

    add logging

--- pages/api/cron/findISBNMatches.js
+++ pages/api/cron/findISBNMatches.js
@@ -94,6 +94,7 @@ const handler = async (req, res) => {
 
     res.status(200).json({ message: "ISBN matches updated successfully" });
   } catch (error) {
+    console.error(error.message);
     res.status(500).json({ message: error.message });
   }
 };
