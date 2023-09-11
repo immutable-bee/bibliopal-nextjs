637ed6fb02755185591bbe41b5b973322c2f8df7
immutable-bee <immutablebee@gmail.com>
2023-09-11 14:22:22 -0600

    update component

--- components/business/BarcodeScanner.js
+++ components/business/BarcodeScanner.js
@@ -1,9 +1,10 @@
 import React, { useRef, useEffect } from "react";
 import Quagga from "quagga";
+
 const BarcodeScanner = ({ onDetected }) => {
   const scannerRef = useRef(null);
 
-  useEffect(() => {
+  const initializeQuagga = () => {
     Quagga.init(
       {
         inputStream: {
@@ -19,6 +20,7 @@ const BarcodeScanner = ({ onDetected }) => {
             left: "0%",
             bottom: "0%",
           },
+          target: scannerRef.current, // Ensuring Quagga uses our ref for rendering
         },
         decoder: {
           readers: ["ean_reader", "ean_8_reader"],
@@ -37,6 +39,47 @@ const BarcodeScanner = ({ onDetected }) => {
       const code = data.codeResult.code;
       onDetected(code);
     });
+  };
+
+  const checkCameraDevices = () => {
+    navigator.mediaDevices.enumerateDevices().then((devices) => {
+      const cameras = devices.filter((device) => device.kind === "videoinput");
+      if (cameras.length === 0) {
+        alert("No camera found on this device.");
+      } else {
+        checkCameraPermission();
+      }
+    });
+  };
+
+  const checkCameraPermission = () => {
+    navigator.permissions.query({ name: "camera" }).then((permissionStatus) => {
+      if (permissionStatus.state === "granted") {
+        initializeQuagga();
+      } else if (permissionStatus.state === "denied") {
+        alert(
+          "You have denied camera access. Please allow it for barcode scanning."
+        );
+      } else {
+        requestCameraAccess();
+      }
+    });
+  };
+
+  const requestCameraAccess = () => {
+    navigator.mediaDevices
+      .getUserMedia({ video: true })
+      .then((stream) => {
+        stream.getTracks().forEach((track) => track.stop()); // Stopping the stream right away
+        initializeQuagga();
+      })
+      .catch((error) => {
+        alert("Error accessing the camera.");
+      });
+  };
+
+  useEffect(() => {
+    checkCameraDevices();
 
     return () => {
       Quagga.offDetected();
