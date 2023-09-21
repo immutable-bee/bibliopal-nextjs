79273e1e4bf906efa58251d2fb5054564897533a
immutable-bee <immutablebee@gmail.com>
2023-09-21 15:49:34 -0600

    add test page

--- pages/test/index.js
+++ pages/test/index.js
@@ -1,7 +1,31 @@
-import OnboardingContainer from "../../components/containers/OnboardingContainer";
+import Header from "@/components/Header";
+import { useState } from "react";
+import BarcodeScanner from "../../components/business/BarcodeScanner";
 
 const TestPage = () => {
-  return <OnboardingContainer />;
+  const [isCameraOpen, setIsCameraOpen] = useState(false);
+
+  const openCamera = () => {
+    setIsCameraOpen(true);
+  };
+
+  return (
+    <div className="bg-[#FEFBE8] min-h-screen">
+      <Header />
+      <div className="flex justify-center mt-20 border border-black">
+        {isCameraOpen ? (
+          <BarcodeScanner />
+        ) : (
+          <button
+            className="px-5 py-3 bg-biblioSeafoam text-white"
+            onClick={openCamera}
+          >
+            Open Camera
+          </button>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default TestPage;
